df001056a2db30cc4a317b2de18038b2391865d9
Justin Dubin <justindubin31@gmail.com>
2024-07-20 14:57:01 -0500

    generalizing

--- astro_api/astro_api.py
+++ astro_api/astro_api.py
@@ -2,10 +2,15 @@ import requests
 import json
 import yaml
 import time
+from enum import Enum
+from typing import Union
 
 BASE_URL = "https://astrosphericpublicaccess.azurewebsites.net/api"
-URL_FORECAST = "GetForecastData_V1"
-URL_SKY = "GetSky_V1"
+
+
+class AstroDtype(Enum):
+    FORECAST = "GetForecastData_V1"
+    SKY_MAP = "GetSky_V1"
 
 
 def fetch_api_key() -> str:
@@ -14,13 +19,22 @@ def fetch_api_key() -> str:
         return api_key
 
 
-def fetch_forecast(latitude: float, longitude: float) -> dict:
-    api_url = BASE_URL + '/' + URL_FORECAST
+def fetch_astro_data(dtype: Union[AstroDtype | str], loc_coords: tuple, ms_since_epoch=None) -> dict:
+    if type(dtype) is str:
+        dtype = AstroDtype[dtype.upper()]
+    if ms_since_epoch is None:
+        ms_since_epoch = round(time.time() * 1000)
+
+    # Build the API URL and construct the parameter container
+    api_url = BASE_URL + "/" + str(dtype.value)
+    latitude, longitude = loc_coords[0], loc_coords[1]
     api_data = {
         "Latitude": latitude,
         "Longitude": longitude,
         "APIKey": fetch_api_key(),
     }
+    if dtype == AstroDtype.SKY_MAP:
+        api_data["MSSinceEpoch"] = ms_since_epoch
 
     # Convert data to JSON format
     json_data_string = json.dumps(api_data)
@@ -31,47 +45,17 @@ def fetch_forecast(latitude: float, longitude: float) -> dict:
 
     # Handle response from API
     if response.status_code == 200:
-        astro_data = json.loads(response.text)
+        response_text = response.text
+        if response.text.startswith('\ufeff'):
+            response_text = response.text[1:]
+        astro_data = json.loads(response_text)
         return astro_data
     else:
+        print(response.content.decode('utf-8'))
         return {}
-
-
-def fetch_sky(latitude: float, longitude: float, ms_since_epoc: int) -> dict:
-    api_url = BASE_URL + '/' + URL_SKY
-    api_data = {
-        "Latitude": latitude,
-        "Longitude": longitude,
-        "MSSinceEpoch": ms_since_epoc,
-        "APIKey": fetch_api_key(),
-    }
-
-    # Convert data to JSON format
-    json_data_string = json.dumps(api_data)
-
-    # Send POST request
-    headers = {"Content-type": "application/json"}
-    response = requests.post(api_url, data=json_data_string, headers=headers)
-
-    # Handle response from API
-    if response.status_code == 200:
-
-        astro_data = json.loads(response.text)
-        # astro_data = response.text
-        return astro_data
-    else:
-        return {}
-    pass
 
 
 LATITUDE = 30.218910
 LONGITUDE = -97.854607
 MS_SINCE_EPOCH = int(time.time() * 1000)
-
-forecast = fetch_forecast(LATITUDE, LONGITUDE)
-print(forecast)
-print()
-
-sky = fetch_sky(LATITUDE, LONGITUDE, MS_SINCE_EPOCH)
-print(sky)
-print()
+fetch_astro_data(dtype=AstroDtype.SKY_MAP, loc_coords=(LATITUDE, LONGITUDE))
